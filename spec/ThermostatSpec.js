e662b01715bc5dcbc77e991b0b390af44ddf82eb
Anindya Bhattacharyya <bat020@gmail.com>
2015-09-29 15:55:53 +0100

    stripped out Jasmine Player

---
+++ spec/ThermostatSpec.js
@@ -0,0 +1,12 @@
+describe('Thermostat', function() {
+
+//   describe("#resume", function() {
+//     it("should throw an exception if song is already playing", function() {
+//       player.play(song);
+//
+//       expect(function() {
+//         player.resume();
+//       }).toThrowError("song is already playing");
+//     });
+//   });
+// });
