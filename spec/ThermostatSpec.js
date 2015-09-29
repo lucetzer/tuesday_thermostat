1d09ae36b9182ae24811efc5ca8ee01a13562417
Anindya Bhattacharyya <bat020@gmail.com>
2015-09-29 16:29:09 +0100

    power saving mode can switch on/off, altering the max appropriately

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,12 +1,43 @@
 describe('Thermostat', function() {
 
-//   describe("#resume", function() {
-//     it("should throw an exception if song is already playing", function() {
-//       player.play(song);
-//
-//       expect(function() {
-//         player.resume();
-//       }).toThrowError("song is already playing");
-//     });
-//   });
-// });
+  var thermo;
+
+  beforeEach(function() {
+    thermo = new Thermostat();
+  });
+
+  describe('properties', function() {
+    it('starts with temp of 20', function() {
+      expect(thermo.temp).toBe(20);
+    });
+    it('starts in power saving mode', function() {
+      expect(thermo.mode).toBe('power saving');
+    });
+    it('has a minimum of 10', function() {
+      expect(thermo.min).toBe(10);
+    });
+    it('starts with maximum of 25', function() {
+      expect(thermo.max).toBe(25);
+    });
+  });
+
+  describe('power saving mode', function(){
+    it('can switch power saving off', function() {
+      thermo.setPowerSaving(false);
+      expect(thermo.mode).toBe('normal');
+    });
+    it('can switch power saving on', function() {
+      thermo.setPowerSaving(true);
+      expect(thermo.mode).toBe('power saving');
+    });
+    it('has max of 25 in power saving mode', function() {
+      thermo.setPowerSaving(true);
+      expect(thermo.max).toBe(25);
+    });
+    it('has max of 32 in normal mode', function() {
+      thermo.setPowerSaving(false);
+      expect(thermo.max).toBe(32);
+    });
+  });
+
+});
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,2 +1,11 @@
-function Thermostat() {
-}
+var Thermostat = function() {
+  this.temp = 20;
+  this.mode = 'power saving';
+  this.min = 10;
+  this.max = 25;
+};
+
+Thermostat.prototype.setPowerSaving = function(value) {
+  this.mode = (value ? 'power saving' : 'normal');
+  this.max = (value ? 25 : 32);
+};
